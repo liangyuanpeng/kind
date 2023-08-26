680750f7b0b72a67908276c337862dfd9e2fd240
Lan Liang <gcslyp@gmail.com>
2023-08-26 15:48:30 +0000

    u

--- pkg/build/nodeimage/buildcontext.go
+++ pkg/build/nodeimage/buildcontext.go
@@ -222,7 +222,8 @@ func (c *buildContext) prePullImagesAndWriteManifests(bits kube.Bits, parsedVers
 		}
 	}
 	requiredImages = append(requiredImages[:n], pauseImage)
-	requiredImages = append(requiredImages, "envoyproxy/envoy:v1.25.1")
+	
+	requiredImages = append(requiredImages, "docker.io/envoyproxy/envoy:v1.25.1")
 
 	if parsedVersion.LessThan(version.MustParseSemantic("v1.24.0")) {
 		if err := configureContainerdSystemdCgroupFalse(cmder, string(containerdConfig)); err != nil {
